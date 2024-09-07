842f804144f4f7b65a748b0f54db6d425f090515
mudan <myudanov223@gmail.com>
2024-09-07 16:33:26 +0300

    edit docker-compose.yaml

--- src/test/java/ru/mudan/CalculatorServiceTests.java
+++ src/test/java/ru/mudan/CalculatorServiceTests.java
@@ -3,7 +3,6 @@ package ru.mudan;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.mudan.dto.DateDTO;
 import ru.mudan.services.CalculatorService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
